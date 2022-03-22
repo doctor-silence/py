0716db64650a982defcba07d7d43d27a20fb7077
admin <admin@MacBook-Pro.local>
2022-03-22 22:53:45 +0300

    add_ip

---
+++ ip.py
@@ -0,0 +1,18 @@
+from urllib import response
+import requests
+
+def get_info_by_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        print(response)
+    except requests.exceptions.ConnectionError:
+        print('[!] Пожалуйста проверьте ваше соединение!')
+
+def main():
+    ip = input('Пожалуйста введите IP: ')
+
+    get_info_by_ip(ip=ip)\
+
+
+if __namr__== '__main__':
+    main()
